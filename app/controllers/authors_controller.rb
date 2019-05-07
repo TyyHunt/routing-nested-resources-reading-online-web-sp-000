f4f6050a7f427a9451ba9af4da640fae44c58b75
Tyler Hunt <tyhunt04@gmail.com>
2019-05-07 17:30:33 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,7 +6,7 @@ class AuthorsController < ApplicationController
 
   def post
     @author = Author.find(params[:id])
- 
+
     @post = Post.find(params[:post_id])
     render template: 'posts/show'
   end
